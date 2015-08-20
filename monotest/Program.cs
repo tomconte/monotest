8c7ac1004359dfab485265638035d53ccf825f2a
Thomas Conte <thomas@conte.com>
2015-08-20 14:01:15 +0200

    Send some Event Hub messages

--- monotest/Program.cs
+++ monotest/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
 
 namespace monotest
 {
 	class MainClass
 	{
+		static string eventHubName = "snappy";
+		static string connectionString = "Endpoint=sb://demohub.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=BOLIBIGWLoIOk1rVy+DqhKS2mnAgx33qCQQuMz03KLg=";
+
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine("Press Ctrl-C to stop the sender process");
+			Console.WriteLine("Press Enter to start now");
+			Console.ReadLine();
+			SendingRandomMessages();
+		}
+
+		static void SendingRandomMessages()
+		{
+			var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+			while (true)
+			{
+				try
+				{
+					var message = Guid.NewGuid().ToString();
+					Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+					eventHubClient.Send(new EventData(System.Text.Encoding.UTF8.GetBytes(message)));
+				}
+				catch (Exception exception)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+					Console.ResetColor();
+				}
+
+				Thread.Sleep(200);
+			}
 		}
 	}
 }
